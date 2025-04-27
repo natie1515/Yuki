83b48ba6bd1175894e6fc2ca9ad2b554e1e0db28
natie1515 <frejulian57@gmail.com>
2025-04-27 08:27:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ from discord.ui import Button, View
 import uuid, os
 from yt_dlp import YoutubeDL
 import io
-import pytz
+from datetime import timedelta
+from datetime import timezone
 
 # Configuración
 BOT_TOKEN = 'MTM1Njc1MTIyMzI0MzQwNzUxMA.GTc8-g.50yQwjeuleAmEJuVZNaK1tcUcauW7v9-gyj2Jo'  # Reemplaza con tu token
